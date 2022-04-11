495d8459497463b3e981b6af39be849f4a8dc971
ULUGBEK2003 <temirovulugbek2003@gmail.com>
2022-04-11 12:12:41 +0500

    modified a few more things

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const ReactRoot = document.getElementById("root");
 const root = createRoot(ReactRoot);
@@ -13,7 +15,4 @@ root.render(
   </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
